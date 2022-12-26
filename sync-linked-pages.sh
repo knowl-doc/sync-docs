cae938525acd881985ea8aa2b018419d14f94a61
ashvin-sharma <sharma.ashvin@gmail.com>
2022-12-26 13:46:14 +0530

    testing precommit hook

--- sync-linked-pages.sh
+++ sync-linked-pages.sh
@@ -65,6 +65,7 @@ cleanup() {
 verify_wget
 verify_tmp
 check_knowl_cli_version
+echo $EVENT_PR_REQUEST
 knowl-cli knowl_cli $EVENT_PR_REQUEST $TEMP_DATA_FILE
 is_pass=$(head -n 1 $TEMP_DATA_FILE)
 rm $TEMP_DATA_FILE
