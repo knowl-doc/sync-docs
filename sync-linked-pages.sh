90f17e753b3c9d58875707291701c2a5b067be0b
ashvin-sharma <sharma.ashvin@gmail.com>
2022-12-26 14:19:22 +0530

    Update sync-linked-pages.sh

--- sync-linked-pages.sh
+++ sync-linked-pages.sh
@@ -66,8 +66,9 @@ cleanup() {
 verify_wget
 verify_tmp
 check_knowl_cli_version
+echo "what's happing here"
 echo $EVENT_PR_REQUEST
-echo $$TEMP_DATA_FILE
+echo $TEMP_DATA_FILE
 ./knowl-cli cli $EVENT_PR_REQUEST $TEMP_DATA_FILE
 is_pass=$(head -n 1 $TEMP_DATA_FILE)
 echo $is_pass 
